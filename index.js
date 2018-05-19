f15ea345b3777acba66c273c5f80c301701d082f
Sav Younts <savyounts@gmail.com>
2018-05-19 21:14:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  var ranks = document.querySelector('.ranked-list')
+  var ranks = document.querySelectorAll('.ranked-list')
   for (var i =0; i<ranks.length; i++){
     ranks[i].innerHTML = ranks[i].innerHTML + n
   }
